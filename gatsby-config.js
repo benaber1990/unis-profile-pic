84157cf4ca26d9c9c9eca1acf4194b0575a123b8
benaber1990 <benaber1990@gmail.com>
2023-09-20 12:46:14 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `unis-profiles`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
